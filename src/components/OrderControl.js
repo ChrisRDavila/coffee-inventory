c372ed0f5caca5765121e32ad06bbe492fd2c934
ChrisRDavila <crossdavila@gmail.com>
2023-12-08 11:56:17 -0800

    add OrderList and NewOrderForm to OrderControl and create state in class module

--- src/components/OrderControl.js
+++ src/components/OrderControl.js
@@ -1,12 +1,26 @@
 import React from "react";
 import OrderList from "./OrderList";
+import NewOrderForm from "./NewOrderForm";
 
-function OrderControl(){
-  return (
-    <React.Fragment>
-      <OrderList />
-    </React.Fragment>
-  );
+class OrderControl extends React.Component {
+  
+    constructor(props) {
+      super(props);
+      this.state = {
+        formVisibleOnPage: false
+      };
+    }
+  
+    
+
+  render(){
+    return (
+      <React.Fragment>
+        <OrderList />
+        <NewOrderForm />
+      </React.Fragment>
+    );
+  }
 }
 
 export default OrderControl;

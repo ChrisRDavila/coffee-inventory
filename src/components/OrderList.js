fe266a091189013dd4233c6ed11636650df5f89f
ChrisRDavila <crossdavila@gmail.com>
2023-12-08 11:50:51 -0800

    add map tp loop through mainOrderList

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -3,13 +3,18 @@ import Order from "./Order";
 
 function OrderList(){
   return (
-    <Order 
-      name={coffee.name}
-      origin={coffee.origin}
-      price={coffee.price}
-      roast={coffee.roast}
-      amount={coffee.amount}
-    />
+    <React.Fragment>
+      <hr/>
+      {mainOrderList.map((order, index) =>
+        <Order
+          name={order.name}
+          origin={order.origin}
+          price={order.price}
+          roast={order.roast}
+          amount={order.amount}
+          key={index}/>
+      )}
+    </React.Fragment>
   );
 }
 
